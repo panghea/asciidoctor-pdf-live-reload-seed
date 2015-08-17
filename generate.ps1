d980de8e53db321534dde9e22c8bf071d6a7bcc3
mon <pangeha.develop@gmail.com>
2015-08-17 11:55:02 +0900

    fix cjk

--- generate.ps1
+++ generate.ps1
@@ -4,7 +4,7 @@ write-host -for Green generating html...
 asciidoctor -b html5 -d book -a "data-uri!" -a "source-highlighter=coderay" -a toc2 -r asciidoctor-diagram  $file
 
 write-host -for Green generating pdf by asciidoctor-pdf with thema ...
-asciidoctor -o ($file -replace ".adoc",".pdf") -a "pdf-fontsdir=fonts" -a "pdf-stylesdir=pdf-style" -a "pdf-style=basic-theme.yml" -b pdf  -a toc  -r asciidoctor-pdf -d book -a "data-uri!" -a "source-highlighter=rouge" -r asciidoctor-diagram  $file
+asciidoctor -o ($file -replace ".adoc",".pdf")  -r asciidoctor-pdf-cjk -a "pdf-fontsdir=fonts" -a "pdf-stylesdir=pdf-style" -a "pdf-style=basic-theme.yml" -b pdf  -a toc  -r asciidoctor-pdf -d book -a "data-uri!" -a "source-highlighter=rouge" -r asciidoctor-diagram  $file
 
 write-host -for Green generated
 
