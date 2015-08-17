e0ba78f8e4397b72891f9660f874311a50b729bb
mon <pangeha.develop@gmail.com>
2015-08-17 10:11:01 +0900

    gulp improve for win

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell')
+var plumber = require('gulp-plumber');
+var convertEncoding = require('gulp-convert-encoding');
 
-    console.log( process.argv.slice(2)[2])
+console.log( process.argv.slice(2)[2])
 
-gulp.task('gen', function() {
-    gulp.src("*.adoc")
-    .pipe(shell(['./generate.sh ' + process.argv.slice(2)[2]]));
-});
+var isWin = /^win/.test(process.platform);
+if (isWin) {
+    gulp.task('gen', function() {
+        gulp.src(  process.argv.slice(2)[2] + ".adoc")
+        .pipe(plumber())
+        .pipe(shell(['powershell ./generate.ps1 ' + process.argv.slice(2)[2]]))
+        .pipe(convertEncoding({to: "utf-8"}));
+    });
+} else {
+    gulp.task('gen', function() {
+        gulp.src(process.argv.slice(2)[2] +".adoc")
+        .pipe(plumber())
+        .pipe(shell(['./generate.sh ' + process.argv.slice(2)[2]]));
+    });
+}
 
 gulp.task('watch', function (callback) {
   gulp.watch(process.argv.slice(2)[2] + ".adoc",['gen'], function (e) {
@@ -16,3 +29,4 @@ gulp.task('watch', function (callback) {
 });
 
 gulp.task('default',['watch']);
+
